ac4b6a348a179b629cf4431d418b58db73a723dd
Hetvirami <159774240+Hetvirami@users.noreply.github.com>
2024-02-20 14:25:40 +0530

    Hello world.cpp

---
+++ Hello world.cpp
@@ -0,0 +1,12 @@
+#includ<iostream>
+using namespace std;
+int main()
+{ 
+cout<<"Hello world";
+return 0;
+}
+
+<!---
+Hetvirami/Hetvirami is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
